a17e79ab6b738b82c789b401febb903f67af4186
FabiRR1 <fabi_bloqueazul16@hotmail.com>
2026-02-20 13:15:43 -0500

    Prueba controlador

--- app/controllers/ContactosController.php
+++ app/controllers/ContactosController.php
@@ -1,16 +1,8 @@
 <?php
 
-namespace App\Controllers;
-use App\Models\Contactos;
-
-class ContactosController extends Controller{
-
     public function index()
 {
     return response()->json([
         "mensaje" => "El controlador funciona correctamente"
     ]);
-}
-
-
 }
